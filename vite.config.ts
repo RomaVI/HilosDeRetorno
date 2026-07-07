8b84be606fccdfc90894da60782ff99b4eb4c0a9
RomaVI <115507047+RomaVI@users.noreply.github.com>
2026-07-07 00:46:00 -0300

    routes v0.0.3

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,14 @@ import { resolve } from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/HilosDeRetorno/',
+  build: {
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
